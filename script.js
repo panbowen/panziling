212d0da73d1e055288bf127ec1c6be3c7f1a0122
panbowen <pbwpbw2008@yahoo.com>
2016-09-16 21:43:01 -0400

    import all the photos and necessary files

---
+++ script.js
@@ -0,0 +1,37 @@
+var app = angular.module('plunker', []);
+
+app.controller('MainCtrl', function($scope) {
+  $scope.items1 = [1,2,3,4,5];
+  $scope.items2 = [1,2,3,4,5,6,7,8,9,10];
+}).directive("owlCarousel", function() {
+	return {
+		restrict: 'E',
+		transclude: false,
+		link: function (scope) {
+			scope.initCarousel = function(element) {
+			  // provide any default options you want
+				var defaultOptions = {
+				};
+				var customOptions = scope.$eval($(element).attr('data-options'));
+				// combine the two options objects
+				for(var key in customOptions) {
+					defaultOptions[key] = customOptions[key];
+				}
+				// init carousel
+				$(element).owlCarousel(defaultOptions);
+			};
+		}
+	};
+})
+.directive('owlCarouselItem', [function() {
+	return {
+		restrict: 'A',
+		transclude: false,
+		link: function(scope, element) {
+		  // wait for the last item in the ng-repeat then call init
+			if(scope.$last) {
+				scope.initCarousel(element.parent());
+			}
+		}
+	};
+}]);
